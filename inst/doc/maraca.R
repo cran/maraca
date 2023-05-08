44162e1fd4d950583e342d654d167889cdd847c5
Monika Huhn <monika.huhn@astrazeneca.com>
2023-05-08 17:20:09 +0000

    version 0.5.0

--- inst/doc/maraca.R
+++ inst/doc/maraca.R
@@ -1,6 +1,7 @@
 ## ----setup, include = FALSE---------------------------------------------------
 knitr::opts_chunk$set(echo = TRUE, collapse = TRUE)
 library(dplyr)
+library(ggplot2)
 library(maraca)
 
 ## ----maraca1, eval = TRUE-----------------------------------------------------
@@ -35,7 +36,9 @@ arm_levels = c(active = "Active", control = "Control")
 
 ## ----maraca7, eval = TRUE-----------------------------------------------------
 mar <- maraca(
-  data, tte_outcomes, continuous_outcome, arm_levels, column_names, compute_win_odds = TRUE
+  data, tte_outcomes, continuous_outcome, arm_levels, column_names, 
+  fixed_followup_days = 3*365,
+  compute_win_odds = TRUE
 )
 
 ## ---- eval = TRUE-------------------------------------------------------------
@@ -55,10 +58,4 @@ p <- plot_maraca(mar, continuous_grid_spacing_x = 20, density_plot_type = "scatt
 p + 
   theme_bw() + scale_color_manual(values=c("#999999", "#E69F00")) + 
   theme(axis.text.x.bottom = element_text(vjust = 0.5, hjust = 0.5))
-
-## ----maraca12, eval = TRUE, fig.width = 7, fig.height = 6, message=FALSE, warning=FALSE----
-plot_tte_components(mar)
-
-## ----maraca13, eval = TRUE, fig.width = 7, fig.height = 6, message=FALSE, warning=FALSE----
-plot_tte_composite(mar)
 
